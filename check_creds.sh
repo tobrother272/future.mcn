3019365084bacb240f50135ff7467d1afde03c57
tobrother272 <tobrother272@gmail.com>
2026-06-24 11:04:11 +0700

    chore: remove temp debug scripts

--- check_creds.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "=== Channel credentials check ==="
-docker exec meridian-postgres psql -U meridian -d meridian -c "SELECT id, email_access, password_enc IS NOT NULL as has_pwd FROM channel WHERE id='C_MQJ60PRI37NB';"
-
-echo "=== CHANNEL_CRED_SECRET check ==="
-docker exec meridian-backend printenv CHANNEL_CRED_SECRET | wc -c
